7dbe351d3f4326da873d9750eb37c1cb99fa1ec7
Mohd Subhan <subhanmohammed631@gmail.com>
2025-12-23 19:57:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import contactRoutes from "./routes/contactRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+app.use(express.json());
+
+app.use("/api", contactRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
