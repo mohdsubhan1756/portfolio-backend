413b84f077e1789e9c268d4c60bb7002d18d0453
mohdsubhan1756 <subhanmohammed631@gmail.com>
2025-12-24 19:38:49 +0530

    Enable CORS with specified origin and methods

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ connectDB();
 
 const app = express();
 
-// app.use(cors({
-//     origin: process.env.FRONTEND_URL,
-//     methods: ["GET", "POST", "PUT", "DELETE"]
-// }));
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
